80aebe2f7bc24bd1ec3674ef4f6cee4f08612c4a
Theo Weller <weller.theodor@gmail.com>
2019-02-21 17:39:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,7 @@
 def prime?(integer)
+  (2..integer - 1).each do |x|
+    return false if (integer % x)==0
+  end
 if integer % 1 == 0 && integer % integer == 0
   true
 elsif integer < 2
@@ -6,8 +9,4 @@ elsif integer < 2
 else
   false
   end
-  (2..integer - 1).each do |x|
-    return false if (integer % x)==0
-  end
-
 end
